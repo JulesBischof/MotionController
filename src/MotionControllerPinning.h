88e0214f56f3e55ec960fb214b40602e03b8175e
julian bischof <julian-bischof@hotmail.com>
2025-02-10 15:35:13 +0100

    added MotionController Pinning

---
+++ src/MotionControllerPinning.h
@@ -0,0 +1,42 @@
+#ifndef MOTIONCONTROLLERPINNIG
+#define MOTIONCONTROLLERPINNIG
+
+/* ==============================================================
+
+    Basic Pinning MotionController - refer schematics
+
+============================================================== */
+
+#define Tx_UART0 0
+#define Rx_UART0 1
+
+#define Tx_UART1 4
+#define Rx_UART1 5
+
+#define DIN_1 2
+#define DIN_2 3
+#define DIN_3 6
+#define DIN_4 7
+
+#define DOUT_1 8
+#define DOUT_2 9
+#define DOUT_3 10
+#define DOUT_4 11
+
+#define GYRO_INT1 12
+
+#define SPI_CS_DRIVER_0 13
+#define SPI_CS_DRIVER_1 17
+#define SPI_MOSI 19
+#define SPI_MISO 16
+#define SPI_SCK 18
+
+#define I2C0_SCK 21
+#define I2C0_SDA 20
+
+#define HCSR04_TRIGGER 22
+#define HCSR04_ECHO 26
+
+#define TXLED_ON 27
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "hardware/i2c.h"
 
+#include "MotionControllerPinning.h"
+
 // void led_task()
 // {
 //     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
@@ -23,14 +25,19 @@ int main()
 {
     stdio_init_all();
 
-    // initialize I2C Bus
+    // -------------   initialize UART  -------------
+
+    // ------------- initialize SPI Bus -------------
+
+    // ------------- initialize I2C Bus -------------
     i2c_init(i2c0, 400e3); // set Baudrate 400kHz
-    i2c_init(i2c1, 400e3);
+    gpio_set_function(I2C0_SCK, GPIO_FUNC_I2C);
+    gpio_set_function(I2C0_SDA, GPIO_FUNC_I2C);
 
 
 
 
-
+    
     // xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
